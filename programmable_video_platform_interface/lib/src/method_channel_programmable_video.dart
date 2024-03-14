0a655b1074b6d6725afb727a1426815f6eec5a66
Martin Trollip <13981100+martintrollip@users.noreply.github.com>
2024-03-14 13:01:06 +0200

    Use initSurfaceAndroidView

--- programmable_video_platform_interface/lib/src/method_channel_programmable_video.dart
+++ programmable_video_platform_interface/lib/src/method_channel_programmable_video.dart
@@ -53,7 +53,7 @@ class MethodChannelProgrammableVideo extends ProgrammableVideoPlatform {
           );
         },
         onCreatePlatformView: (params) {
-          return PlatformViewsService.initAndroidView(
+          return PlatformViewsService.initSurfaceAndroidView(
             id: params.id,
             viewType: viewType,
             layoutDirection: TextDirection.ltr,
